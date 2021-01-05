4365cf8cb94e245f24cfea44b98c1b67718422a0
Rick Bunnell <rickbunnell120@hotmail.com>
2021-01-05 12:14:30 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// select all the buttons
+const buttons = document.querySelectorAll('button');
+// select the <input type="text" class="display" disabled> element
+const display = document.querySelector('.display');
+
+// add eventListener to each button
+buttons.forEach(function(button) {
+  button.addEventListener('click', calculate);
+});
+
+// calculate function
+function calculate(event) {
+  // current clicked buttons value
+  const clickedButtonValue = event.target.value;
+
+  if (clickedButtonValue === '=') {
+    // check if the display is not empty then only do the calculation
+    if (display.value !== '') {
+      // calculate and show the answer to display
+      display.value = eval(display.value);
+    }
+  } else if (clickedButtonValue === 'C') {
+    // clear everything on display
+    display.value = '';
+  } else {
+    // otherwise concatenate it to the display
+    display.value += clickedButtonValue;
+  }
+}
+
+// Get the hamburger menu link icon
+const menuButton = document.getElementById('icon').addEventListener('click', menuMessage);
+
+// Message displayed when the hamburger menu icon is clicked  
+function menuMessage() {
+    display.value = 'Crashed..';
+  }
